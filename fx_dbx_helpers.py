b4263f46fb3dcb145b7051cab1fa85f6ef7510e6
Andrew-Tolbert <tolberta14@gmail.com>
2022-12-14 10:20:03 -0500

    Commiting new display_html syntax

--- fx_dbx_helpers.py
+++ fx_dbx_helpers.py
@@ -5,13 +5,20 @@ Some Repurposed (not original author) and Some Original - flagged in comments
 """
 
 # REPRUPOSED Use to Embed Google Slides in dbx notebooks 
-def display_slide(slide_id, slide_number):
-  displayHTML(f'''
-  <div style="width:1150px; margin:auto">
-  <iframe
-    src="https://docs.google.com/presentation/d/{slide_id}/embed?slide={slide_number}"
-    frameborder="0"
-    width="1150"
-    height="683"
-  ></iframe></div>
-  ''')
+def display_html_v2(html: str) -> None:
+    """
+    Use databricks displayHTML from an external package
+    
+    Args:
+    - html : html document to display
+    """
+    import inspect
+    for frame in inspect.getouterframes(inspect.currentframe()):
+        global_names = set(frame.frame.f_globals)
+        # Use multiple functions to reduce risk of mismatch
+        if all(v in global_names for v in ["displayHTML", "display", "spark"]):
+            return frame.frame.f_globals["displayHTML"](html)
+    raise EnvironmentError(
+        "Unable to detect displayHTML function"
+    )
+
