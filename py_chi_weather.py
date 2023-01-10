a849b67a399bf75e526807e3173e1d5ca7a06b12
Andrew-Tolbert <tolberta14@gmail.com>
2023-01-10 16:20:39 -0500

    new projects

---
+++ py_chi_weather.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+data_ws = 'https://data.cityofchicago.org/api/views/qmqz-2xku/rows.json'
+
+data_weather = 'https://data.cityofchicago.org/api/views/k7hf-8y75/rows.json'
+
